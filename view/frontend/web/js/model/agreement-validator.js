59bae21d291d717cca9f0715d4fe62ca9997e39c
david-fiaty-cko <david.fiaty@checkout.com>
2017-05-03 09:58:00 +0100

    TOS Validation
    Added a model folder and a file for TOS validation.

---
+++ view/frontend/web/js/model/agreement-validator.js
@@ -0,0 +1,45 @@
+
+define(
+    [
+        'jquery',
+        'mage/validation'
+    ],
+    function ($) {
+        'use strict';
+        var checkoutConfig = window.checkoutConfig,
+            agreementsConfig = checkoutConfig ? checkoutConfig.checkoutAgreements : {};
+
+        var agreementsInputPath = '.payment-method._active div.checkout-agreements input';
+
+        return {
+            /**
+             * Validate checkout agreements
+             *
+             * @returns {boolean}
+             */
+            validate: function() {
+
+                if (!agreementsConfig.isEnabled) {
+                    return true;
+                }
+
+                if ($(agreementsInputPath).length == 0) {
+                    return true;
+                }
+
+                return $('#co-payment-form').validate({
+                    errorClass: 'mage-error',
+                    errorElement: 'div',
+                    meta: 'validate',
+                    errorPlacement: function (error, element) {
+                        var errorPlacement = element;
+                        if (element.is(':checkbox') || element.is(':radio')) {
+                            errorPlacement = element.siblings('label').last();
+                        }
+                        errorPlacement.after(error);
+                    }
+                }).element(agreementsInputPath);
+            }
+        }
+    }
+);
