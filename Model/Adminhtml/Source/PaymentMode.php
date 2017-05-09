60587e84080ba625b2ac91f752fbcfd586f3a12f
david-fiaty-cko <david.fiaty@checkout.com>
2017-05-09 12:42:45 +0100

    Payment mode
    Added a class to display the available payment modes in the module
    config in admin.

---
+++ Model/Adminhtml/Source/PaymentMode.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CheckoutCom\Magento2\Model\Adminhtml\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class PaymentMode implements ArrayInterface {
+
+    const MODE_CARDS = 'cards';
+    const MODE_MIXED = 'mixed';
+    const MODE_LOCAL = 'localpayments';
+
+    /**
+     * Possible environment types
+     *
+     * @return array
+     */
+    public function toOptionArray() {
+        return [
+            [
+                'value' => self::MODE_CARDS,
+                'label' => 'Cards',
+            ],
+            [
+                'value' => self::MODE_MIXED,
+                'label' => 'Mixed'
+            ],
+            [
+                'value' => self::MODE_LOCAL,
+                'label' => 'Local payments'
+            ] 
+        ];
+    }         
+
+}
