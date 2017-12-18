d78b270591625819d2ca586544855bc6d7017063
Elvin Risti <elvin.risti@vaimo.com>
2017-12-18 09:24:21 +0200

    Remove view/frontend/layout/sales_* due overriding core functionality by 3rd party module which should not happen.

--- Block/Order/Info.php
+++ Block/Order/Info.php
@@ -17,93 +17,20 @@ use Magento\Payment\Helper\Data as PaymentHelper;
 use Magento\Sales\Model\Order\Address\Renderer as AddressRenderer;
 
 /**
+ * @obsolete
+ *
  * Invoice view comments form
  */
-class Info extends \Magento\Framework\View\Element\Template
+class Info extends \Magento\Sales\Block\Order\Info
 {
     /**
-     * @var string
+     * @obsolete
      */
-    protected $_template = 'order/info.phtml';
-
-    /**
-     * Core registry
-     *
-     * @var \Magento\Framework\Registry
-     */
-    protected $coreRegistry = null;
-
-    /**
-     * @var \Magento\Payment\Helper\Data
-     */
-    protected $paymentHelper;
-
-    /**
-     * @var AddressRenderer
-     */
-    protected $addressRenderer;
-
-    /**
-     * @param TemplateContext $context
-     * @param Registry $registry
-     * @param PaymentHelper $paymentHelper
-     * @param AddressRenderer $addressRenderer
-     * @param array $data
-     */
-    public function __construct(
-        TemplateContext $context,
-        Registry $registry,
-        PaymentHelper $paymentHelper,
-        AddressRenderer $addressRenderer,
-        array $data = []
-    ) {
-        $this->addressRenderer = $addressRenderer;
-        $this->paymentHelper = $paymentHelper;
-        $this->coreRegistry = $registry;
-        $this->_isScopePrivate = true;
-        parent::__construct($context, $data);
-    }
-
-    /**
-     * @return void
-     */
-    protected function _prepareLayout()
+    protected function _construct()
     {
-        $this->pageConfig->getTitle()->set(__('Order # %1', $this->getOrder()->getRealOrderId()));
-        $infoBlock = $this->paymentHelper->getInfoBlock($this->getOrder()->getPayment(), $this->getLayout());
-        $this->setChild('payment_info', $infoBlock);
-    }
+        // we override to keep the translation context correct
+        $this->setModuleName($this->extractModuleName(\Magento\Sales\Block\Order\Info::class));
 
-    /**
-     * @return string
-     */
-    public function getPaymentInfoHtml()
-    {
-        $order = $this->coreRegistry->registry('current_order');
-        $payment = $order->getPayment();
-        $method = $payment->getMethodInstance();
-        
-        return $method->getTitle();
-    }
-
-    /**
-     * Retrieve current order model instance
-     *
-     * @return \Magento\Sales\Model\Order
-     */
-    public function getOrder()
-    {
-        return $this->coreRegistry->registry('current_order');
-    }
-
-    /**
-     * Returns string with formatted address
-     *
-     * @param Address $address
-     * @return null|string
-     */
-    public function getFormattedAddress(Address $address)
-    {
-        return $this->addressRenderer->format($address, 'html');
+        parent::_construct();
     }
 }
