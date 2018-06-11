4fd5db1cde9a1ca7b0f97cae429f5c0232b0757a
david-fiaty-cko <david.fiaty@checkout.com>
2018-06-11 17:45:18 +0100

    Improve error messages display
    Added an improvement for error messages display in case of incorred card data provided, with redirection to the cart for a new attempt.

--- view/frontend/web/js/view/payment/response-strategy.js
+++ view/frontend/web/js/view/payment/response-strategy.js
@@ -13,25 +13,9 @@ define([
     'use strict';
 
     return function(response) {
-        var
-            deferred = $.Deferred(),
-            html = $.parseHTML(response),
-            formId = 'cko-form-redirection';
+        var deferred = $.Deferred();
 
-        if(html[1] !== undefined) {
-            var $form = $(html[1]);
-
-            if($form.length && $form.attr('name') === 'ckoform') {
-                $form.attr('id', formId).attr('styles', 'display:none');
-
-                $('body').append( $form );
-                $('#' + formId).submit();
-
-                return deferred.reject();
-            }
-        }
-
-        return deferred.resolve();
+        //return deferred.resolve();
     }
 
 });
