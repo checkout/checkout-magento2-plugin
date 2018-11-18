5b8b11cdc9093eacacfdbef8659467d48c0096d4
david-fiaty-cko <david.fiaty@checkout.com>
2018-11-18 15:23:51 +0000

    Apple Pay controller fix
    Fixed wrong namespace for validation controller.

--- Controller/Payment/ApplePay/Validation.php
+++ Controller/Payment/ApplePay/Validation.php
@@ -8,7 +8,7 @@
  * License GNU/GPL V3 https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 
-namespace CheckoutCom\Magento2\Controller\Payment;
+namespace CheckoutCom\Magento2\Controller\Payment\ApplePay;
 
 use Magento\Framework\App\Action\Context;
 use CheckoutCom\Magento2\Gateway\Config\Config as GatewayConfig;
