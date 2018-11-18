3004fd12cce4fef0ae30f6fd5e6c738823c17564
david-fiaty-cko <david.fiaty@checkout.com>
2018-11-18 15:11:40 +0000

    Apple Pay place order
    Removed unused functions from Apple Pay place order controller.

--- Controller/Payment/ApplePay/PlaceOrder.php
+++ Controller/Payment/ApplePay/PlaceOrder.php
@@ -92,34 +92,9 @@ class PlaceOrder extends Action {
             'quote' => $this->checkoutSession->getQuote()
         );
 
-        $order = null;
-        if (isset($this->customerSession->getData('checkoutSessionData')['orderTrackId'])) {
-            $order = $this->orderInterface->loadByIncrementId($this->customerSession->getData('checkoutSessionData')['orderTrackId']);
-        }
-
-        if ($order) {
-            $this->updateOrder($params, $order);
-        }
-        else {
-            $this->createOrder($params);
-        }
-    }
-
-    public function updateOrder($params, $order) {
-
-        // Create the charge for order already placed
-        $updateSuccess = $this->tokenChargeService->sendChargeRequest($params['cardToken'], $order);
-
-        // Update payment data
-        $order = $this->updatePaymentData($order);
         
-        // 3D Secure redirection if needed
-        if($this->isUrl($this->checkoutSession->get3DSRedirect())) {
-            $this->place3DSecureRedirectUrl();
-            exit();
-        }
-
-        return $this->_redirect('checkout/onepage/success', ['_secure' => true]);
+        // If charge is successful, create order
+        $this->createOrder($params);
     }
 
     public function createOrder($params) {
@@ -147,12 +122,6 @@ class PlaceOrder extends Action {
              */
             $this->orderService->execute($params['quote'], $params['cardToken'], array(true));
 
-            // 3D Secure redirection if needed
-            if ($this->isUrl($this->checkoutSession->get3DSRedirect())) {
-                $this->place3DSecureRedirectUrl();
-                exit();
-            }
-
             return $this->_redirect('checkout/onepage/success', ['_secure' => true]);
 
         } catch (\Exception $e) {
@@ -160,40 +129,5 @@ class PlaceOrder extends Action {
         }
 
         return $this->_redirect('checkout/cart', ['_secure' => true]);
-    }
-
-    public function updatePaymentData($order) {
-        // Load payment object
-        $payment = $order->getPayment();
-
-        // Set the payment method, previously "substitution" for pre auth order creation
-        $payment->setMethod(ConfigProvider::CODE); 
-        $payment->save();
-        $order->save();
-
-        return $order;
-    }
-
-    /**
-     * Listens to a session variable set in Gateway/Response/ThreeDSecureDetailsHandler.php.
-     *
-     * @return \Magento\Framework\Controller\Result\Redirect
-     */
-    public function place3DSecureRedirectUrl() {
-        $url = $this->checkoutSession->get3DSRedirect();
-        $this->checkoutSession->uns3DSRedirect();
-
-        echo '<script type="text/javascript">';
-        echo 'function waitForElement() {';
-        echo 'var redirectUrl = "' . $url . '";';
-        echo 'if (redirectUrl.length != 0){ window.location.replace(redirectUrl); }';
-        echo 'else { setTimeout(waitForElement, 250); }';
-        echo '} ';
-        echo 'waitForElement();';
-        echo '</script>';
-    }
-
-    public function isUrl($url) {
-        return filter_var($url, FILTER_VALIDATE_URL) === false ? false : true;
     }
 }
