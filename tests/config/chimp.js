4c9de82a324149cd61d6391b190142ef54fc1651
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2019-02-05 13:04:02 +0000

    Update Chrome Interaction

--- tests/config/chimp.js
+++ tests/config/chimp.js
@@ -1,19 +1,18 @@
-
 module.exports = {
-  path: './features',
-  screenshotsPath: './screenshots',
-  screenshotsOnError: true,
-  saveScreenshotsToDisk: true,
-  saveScreenshotsToReport: true,
-  webdriverio: {
-    logLevel: 'silent',
-    screenshotPath: './screenshots',
-    desiredCapabilities: {
-      chromeOptions: {
-        args: ['headless', 'disable-gpu'],
-      },
-      isHeadless: true,
-    },
-    debug: true,
-  },
+	path: './features',
+	screenshotsPath: './screenshots',
+	screenshotsOnError: true,
+	saveScreenshotsToDisk: true,
+	saveScreenshotsToReport: true,
+	webdriverio: {
+		logLevel: 'silent',
+		screenshotPath: './screenshots',
+		desiredCapabilities: {
+			chromeOptions: {
+				args: [ 'headless', 'disable-gpu', '--no-sandbox' ]
+			},
+			isHeadless: true
+		},
+		debug: true
+	}
 };
