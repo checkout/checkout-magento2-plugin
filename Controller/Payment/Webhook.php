8db97f6dbb4aefb2ce65b854105c13117820a358
david-fiaty-cko <david.fiaty@checkout.com>
2019-04-24 09:45:33 +0100

    Added a base Webhook Controller

---
+++ Controller/Payment/Webhook.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CheckoutCom\Magento2\Controller\Payment;
+
+class Webhook extends \Magento\Framework\App\Action\Action {
+    
+	/**
+     * PlaceOrder constructor
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context
+    )
+    {
+        parent::__construct($context);
+    }
+
+    /**
+     * Handles the controller method.
+     */
+    public function execute() {
+        
+    }
+}
