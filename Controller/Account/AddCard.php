911ccc62c5008f1aad5add765586b10a3a754cc9
david-fiaty-cko <david.fiaty@checkout.com>
2019-04-28 15:45:58 +0100

    Added the add card controller

---
+++ Controller/Account/AddCard.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Checkout.com Magento 2 Payment module (https://www.checkout.com)
+ *
+ * Copyright (c) 2017 Checkout.com (https://www.checkout.com)
+ * Author: David Fiaty | integration@checkout.com
+ *
+ * License GNU/GPL V3 https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+namespace CheckoutCom\Magento2\Controller\Account;
+
+class AddCard extends \Magento\Framework\App\Action\Action {
+
+    /**
+     * @var Tools
+     */
+    protected $tools;
+
+    public function __construct(
+        Magento\Framework\App\Action\Context $context
+    ) {
+        parent::__construct($context);
+    }
+
+    /**
+     * Handles the controller method.
+     */
+    public function execute() {
+        // Force login
+        $this->tools->checkLoggedIn();
+
+        // Display the page
+        $this->_view->loadLayout(); 
+        $this->_view->renderLayout(); 
+    } 
+}
