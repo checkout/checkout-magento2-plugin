a6a0d83f8affe9073dac8bab52ef1e7eac6c3aed
david-fiaty-cko <david.fiaty@checkout.com>
2019-05-12 12:51:13 +0100

    Implemented void and refund actions for all methods

--- Model/Methods/AlternativePaymentMethod.php
+++ Model/Methods/AlternativePaymentMethod.php
@@ -296,4 +296,36 @@ class AlternativePaymentMethod extends Method
         return new PoliSource();
     }
 
+    public function void(\Magento\Payment\Model\InfoInterface $payment)
+    {
+        // Check the status
+        if (!$this->canVoid()) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('The void action is not available.'));
+        }
+
+        // Process the void request
+        $response = $this->apiHandler->voidTransaction($payment);
+        if (!$response || !$response->isSuccessful()) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('The void request could not be processed.'));
+        }
+
+        return $this;
+    }
+
+    public function refund(\Magento\Payment\Model\InfoInterface $payment, $amount)
+    {
+        // Check the status
+        if (!$this->canRefund()) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('The refund action is not available.'));
+        }
+
+        // Process the refund request
+        $response = $this->apiHandler->refundTransaction($payment, $amount);
+        if (!$response || !$response->isSuccessful()) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('The refund request could not be processed.'));
+        }
+
+        return $this;
+    }
+
 }
