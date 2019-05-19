f5abcff84caeee9347942898d1b3a26dbc196497
david-fiaty-cko <david.fiaty@checkout.com>
2019-05-19 03:18:37 +0100

    Coompleted the instant purchase place order feature

--- Controller/Button/PlaceOrder.php
+++ Controller/Button/PlaceOrder.php
@@ -13,6 +13,11 @@ namespace CheckoutCom\Magento2\Controller\Button;
 class PlaceOrder extends \Magento\Framework\App\Action\Action
 {
     /**
+     * @var JsonFactory
+     */
+    protected $jsonFactory;
+
+    /**
      * @var QuoteManagement
      */
     protected $quoteManagement;
@@ -42,6 +47,7 @@ class PlaceOrder extends \Magento\Framework\App\Action\Action
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory,
         \Magento\Quote\Model\QuoteManagement $quoteManagement,
         \Magento\Catalog\Model\Product $productModel,
         \Magento\InstantPurchase\Model\QuoteManagement\ShippingConfiguration $shippingConfiguration,
@@ -50,6 +56,7 @@ class PlaceOrder extends \Magento\Framework\App\Action\Action
     ) {
         parent::__construct($context);
 
+        $this->jsonFactory = $jsonFactory;
         $this->quoteManagement = $quoteManagement;
         $this->productModel = $productModel;
         $this->shippingConfiguration = $shippingConfiguration;
@@ -66,47 +73,75 @@ class PlaceOrder extends \Magento\Framework\App\Action\Action
         $billingId = (int) $this->getRequest()->getParam('instant_purchase_billing_address');
         $shippingId = (int) $this->getRequest()->getParam('instant_purchase_shipping_address');
         $publicHash = $this->getRequest()->getParam('instant_purchase_payment_token');
-        
-        // Create the quote
-        $quote = $this->quoteHandler->createQuote();
-        $quote = $this->quoteHandler->addItems(
-            $quote, 
-            [
-                [
-                    'product_id' => $productId,
-                    'qty' => $quantity,
-                    'super_attribute' => $attributes
-                ]
-            ]
-        );
-        
-        // Set the billing address
-        $billingAddress = $this->addressManager->load($billingId);
-        $quote->getBillingAddress()->addData($billingAddress->getData());
 
-        // Set the shipping address and method
-        $shippingAddress = $this->addressManager->load($shippingId);
-        $quote->getShippingAddress()
+        try {
+            // Create the quote
+            $quote = $this->quoteHandler->createQuote();
+            $quote = $this->quoteHandler->addItems(
+                $quote,
+                [
+                    [
+                        'product_id' => $productId,
+                        'qty' => $quantity,
+                        'super_attribute' => $attributes
+                    ]
+                ]
+            );
+        
+            // Set the billing address
+            $billingAddress = $this->addressManager->load($billingId);
+            $quote->getBillingAddress()->addData($billingAddress->getData());
+
+            // Set the shipping address and method
+            $shippingAddress = $this->addressManager->load($shippingId);
+            $quote->getShippingAddress()
             ->addData($shippingAddress->getData())
             ->setCollectShippingRates(true)
             ->collectShippingRates()
             ->setShippingMethod('flatrate_flatrate');
 
-        // Set payment
-        $quote->setPaymentMethod('checkoutcom_vault');
-        $quote->setInventoryProcessed(false);
-        $quote->save();
-        $quote->getPayment()->importData(
-            ['method' => 'checkoutcom_vault']
+            // Set payment
+            $quote->setPaymentMethod('checkoutcom_vault');
+            $quote->setInventoryProcessed(false);
+            $quote->save();
+            $quote->getPayment()->importData(
+                ['method' => 'checkoutcom_vault']
+            );
+
+            // Save the quote
+            $quote->collectTotals()->save();
+
+            // Create the order
+            $order = $this->quoteManagement->submit($quote);
+
+            // Return the response
+            $message = __('Your order number is: %1.', $order->getIncrementId());
+        } catch (\Exception $e) {
+            return $this->createResponse(
+                $e->getMessage(),
+                false
+            );
+        }
+
+        return $this->createResponse($message, true);
+    }
+
+    /**
+     * Creates response with a operation status message.
+     */
+    private function createResponse(string $message, bool $successMessage): JsonResult
+    {
+        $result = $this->jsonFactory->create()->setData(
+            ['response' => $message]
         );
 
-        // Save the quote
-        $quote->collectTotals()->save();
+        if ($successMessage) {
+            $this->messageManager->addSuccessMessage($message);
+        } else {
+            $this->messageManager->addErrorMessage($message);
+        }
 
-        // Create the order
-        $order = $this->quoteManagement->submit($quote);
-
-        exit();
+        return $result;
     }
 
 }
