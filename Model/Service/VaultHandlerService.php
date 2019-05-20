f5ca34f3d5ebc6cb2fbf48c1a479aff11fc10e28
david-fiaty-cko <david.fiaty@checkout.com>
2019-05-20 17:30:46 +0100

    Removed a string in the token formatter

--- Model/Service/VaultHandlerService.php
+++ Model/Service/VaultHandlerService.php
@@ -346,8 +346,7 @@ class VaultHandlerService {
 
         // Return the formatted token
         return sprintf(
-            '%s: %s, %s: %s (%s: %s)',
-            __('Card type'),
+            '%s, %s: %s (%s: %s)',
             $this->cardHandler->getCardScheme($details['type']),
             __('ending'),
             $details['maskedCC'],
