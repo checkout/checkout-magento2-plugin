a0848c3a60f5e8945bfea51eb4eff28a73ed1816
david-fiaty-cko <david.fiaty@checkout.com>
2019-05-21 19:12:09 +0100

    Implemented the verify and fail controllers

--- Controller/Payment/Fail.php
+++ Controller/Payment/Fail.php
@@ -3,27 +3,43 @@
 namespace CheckoutCom\Magento2\Controller\Payment;
 
 class Fail extends \Magento\Framework\App\Action\Action {
-    
     /**
-     * @var JsonFactory
+     * @var CheckoutApi
      */
-    protected $jsonFactory;
+    protected $apiHandler;
 
 	/**
      * PlaceOrder constructor
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
+        \CheckoutCom\Magento2\Model\Service\ApiHandlerService $apiHandler
     )
     {
         parent::__construct($context);
+
+        $this->apiHandler = $apiHandler;
     }
 
     /**
      * Handles the controller method.
      */
     public function execute() {
-        
+        try {
+            // Get the session id
+            $sessionId = $this->getRequest()->getParam('cko-session-id', null);
+            if ($sessionId) {
+                // Get the payment details
+                $response = $this->apiHandler->getPaymentDetails($sessionId);
+                
+                // Todo - Display the gateway error message from $response if debug mode is on
+                $this->messageManager->addErrorMessage(__('The transaction could not be processed.'));  
+            }
+        } catch (\Exception $e) {
+            // Todo - Log and error message if debug mode is on
+        }      
+
+        // Return to the cart
+        return $this->_redirect('checkout/cart', ['_secure' => true]);
     }
 }
