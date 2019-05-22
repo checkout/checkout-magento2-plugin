31c6bb32830a0c86af1e0b881600fbeb5570838f
david-fiaty-cko <david.fiaty@checkout.com>
2019-05-22 18:24:21 +0100

    Added the Frames form theme parameter handling

--- view/frontend/web/js/view/payment/method-renderer/checkoutcom_card_payment.js
+++ view/frontend/web/js/view/payment/method-renderer/checkoutcom_card_payment.js
@@ -81,6 +81,7 @@ define([
                     containerSelector: '.frames-container',
                     debugMode: self.getValue('debug'),
                     localisation: self.getValue('language_fallback'),
+                    theme: self.getValue('form_theme'),
                     cardValidationChanged: function() {
                         if (Frames.isCardValid() && Utilities.getBillingAddress() != null) {
                             Utilities.allowPlaceOrder(self.buttonId, true);
