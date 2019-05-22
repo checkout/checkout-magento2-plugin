1bd9f6f5e956cf8bc64dea1ede822cf421de980f
david-fiaty-cko <david.fiaty@checkout.com>
2019-05-22 11:41:11 +0100

    Added the instant purchase display check

--- Model/InstantPurchase/AvailabilityChecker.php
+++ Model/InstantPurchase/AvailabilityChecker.php
@@ -5,10 +5,38 @@ namespace CheckoutCom\Magento2\Model\InstantPurchase;
 class AvailabilityChecker implements \Magento\InstantPurchase\PaymentMethodIntegration\AvailabilityCheckerInterface
 {
     /**
+     * @var Config
+     */
+    protected $config;
+
+
+    /**
+     * ConfigAlternativePayments  constructor
+     */
+    public function __construct(
+        \CheckoutCom\Magento2\Gateway\Config\Config $config
+    )
+    {
+        $this->configLoader = $config;
+    }
+
+    /**
      * @inheritdoc
      */
     public function isAvailable(): bool
     {
-        return true;
+        // Get the vault state
+        $vaultEnabled = $this->config->getValue(
+            'active',
+            'checkoutcom_vault'
+        );
+
+        // Get the instant purchase state
+        $instantPurchaseEnabled = $this->config->getValue(
+            'instant_purchase_enabled',
+            'checkoutcom_vault'
+        );
+
+        return $vaultEnabled && $instantPurchaseEnabled;
     }
 }
