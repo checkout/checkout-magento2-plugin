0d7e2c4737bc88015f71a1b58f8e359d21525e26
david-fiaty-cko <david.fiaty@checkout.com>
2019-05-27 14:59:28 +0100

    Added logging code

--- Controller/Webhook/Callback.php
+++ Controller/Webhook/Callback.php
@@ -21,6 +21,11 @@ class Callback extends \Magento\Framework\App\Action\Action {
     protected $jsonFactory;
 
     /**
+     * @var OrderRepositoryInterface
+     */
+    protected $orderRepository;
+
+    /**
      * @var apiHandler
      */
     protected $apiHandler;
@@ -51,6 +56,7 @@ class Callback extends \Magento\Framework\App\Action\Action {
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\Controller\Result\JsonFactory $jsonFactory,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
         \CheckoutCom\Magento2\Model\Service\apiHandlerService $apiHandler,
         \CheckoutCom\Magento2\Model\Service\OrderHandlerService $orderHandler,
         \CheckoutCom\Magento2\Model\Service\QuoteHandlerService $quoteHandler,
@@ -61,6 +67,7 @@ class Callback extends \Magento\Framework\App\Action\Action {
         parent::__construct($context);
 
         $this->jsonFactory = $jsonFactory;
+        $this->orderRepository = $orderRepository;
         $this->apiHandler = $apiHandler;
         $this->orderHandler = $orderHandler;
         $this->quoteHandler = $quoteHandler;
@@ -92,6 +99,11 @@ class Callback extends \Magento\Framework\App\Action\Action {
                     $logger->addWriter($writer);
                     $logger->info(print_r($response, 1));
 
+                    $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/isValid.log');
+                    $logger = new \Zend\Log\Logger();
+                    $logger->addWriter($writer);
+                    $logger->info(print_r($this->apiHandler->isValidResponse($response), 1));
+
                     if ($this->apiHandler->isValidResponse($response)) {
                         // Process the order
                         $order = $this->orderHandler->processOrder(
@@ -100,18 +112,29 @@ class Callback extends \Magento\Framework\App\Action\Action {
                             true
                         );
 
-                        // Capture
+                        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/orderid.log');
+                        $logger = new \Zend\Log\Logger();
+                        $logger->addWriter($writer);
+                        $logger->info(print_r($order->getId(), 1));
+
+                        // Handle the transaction
                         $this->transactionHandler->createTransaction(
                             $order,
                             $this->getNeededTransaction($payload->type),
                             $response
                         );
+
+                        // Save the order
+                        $order = $this->orderRepository->save($order);
                     }
                 }
             }
-   
         } catch (\Exception $e) {
 
+            $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/catch.log');
+            $logger = new \Zend\Log\Logger();
+            $logger->addWriter($writer);
+            $logger->info(print_r($e->getMessage(), 1));
         }     
 
         exit();
