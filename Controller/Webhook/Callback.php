6098ed63caecdc04eca217a6bcfddf71fff10d75
david-fiaty-cko <david.fiaty@checkout.com>
2019-05-27 02:42:42 +0100

    Added base logic for the webook handlig

--- Controller/Webhook/Callback.php
+++ Controller/Webhook/Callback.php
@@ -2,6 +2,8 @@
 
 namespace CheckoutCom\Magento2\Controller\Webhook;
 
+use Magento\Sales\Model\Order\Payment\Transaction;
+
 class Callback extends \Magento\Framework\App\Action\Action {
     
     /**
@@ -9,31 +11,88 @@ class Callback extends \Magento\Framework\App\Action\Action {
      */
     protected $jsonFactory;
 
+    /**
+     * @var apiHandler
+     */
+    protected $apiHandler;
+
+    /**
+     * @var OrderHandlerService
+     */
+    protected $orderHandler;
+
+    /**
+     * @var QuoteHandlerService
+     */
+    protected $quoteHandler;
+
+    /**
+     * @var TransactionHandlerService
+     */
+    protected $transactionHandler;
+
+    /**
+     * @var Config
+     */
+    protected $config;
+
 	/**
      * Callback constructor
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
+        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory,
+        \CheckoutCom\Magento2\Model\Service\apiHandlerService $apiHandler,
+        \CheckoutCom\Magento2\Model\Service\OrderHandlerService $orderHandler,
+        \CheckoutCom\Magento2\Model\Service\QuoteHandlerService $quoteHandler,
+        \CheckoutCom\Magento2\Model\Service\TransactionHandlerService $transactionHandler,
+        \CheckoutCom\Magento2\Gateway\Config\Config $config
     )
     {
         parent::__construct($context);
 
         $this->jsonFactory = $jsonFactory;
+        $this->apiHandler = $apiHandler;
+        $this->orderHandler = $orderHandler;
+        $this->quoteHandler = $quoteHandler;
+        $this->transactionHandler = $transactionHandler;
+        $this->config = $config;
     }
 
     /**
      * Handles the controller method.
      */
     public function execute() {
-        // Get the post data
-        $postData = $this->getRequest()->getPostValue();
+        try {
+            if ($this->config->isValidAuth()) {
+                // Get the post data
+                $payload = json_decode($this->getRequest()->getContent());
+                if (isset($payload->data->id)) {
+                    // Get the payment details
+                    $response = $this->apiHandler->getPaymentDetails($payload->data->id);
 
+                    // Get the order
+                    $order = $this->orderHandler->getOrder(
+                        ['reserved_order_id' => $response->reference]
+                    );
 
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/webhook.log');
-		$logger = new \Zend\Log\Logger();
-		$logger->addWriter($writer);
-        $logger->info(print_r($postData, 1));
+                    // Process the webhook tasks
+                    if ($this->orderHandler->isOrder($order) && $this->apiHandler->isValidResponse($response)) {
+                        // Capture
+                        $this->transactionHandler->createTransaction
+                        (
+                            $order,
+                            Transaction::TYPE_AUTH,
+                            $paymentData
+                        );
+                    }
+
+                }
+            }
+   
+        } catch (\Exception $e) {
+
+        }     
 
         exit();
         
