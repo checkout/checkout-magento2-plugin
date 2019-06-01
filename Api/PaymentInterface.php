4e932ef2b285a8ba24a809bd3d9abb846341d46e
david-fiaty-cko <david.fiaty@checkout.com>
2019-06-01 22:30:08 +0100

    Added doc blocks for the API charge method

--- Api/PaymentInterface.php
+++ Api/PaymentInterface.php
@@ -2,5 +2,13 @@
 namespace CheckoutCom\Magento2\Api;
  
 interface PaymentInterface {
+
+    /**
+     * Performs a charge from an external request.
+     *
+     * @api
+     * @param mixed $data.
+     * @return int
+     */
     public function charge($data);
 }
