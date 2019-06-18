e5616de69681d20070a9561f17f88c8213164258
david-fiaty-cko <david.fiaty@checkout.com>
2019-06-18 14:07:33 +0100

    Removed an uneeded override

--- Block/Apm/Form.php
+++ Block/Apm/Form.php
@@ -22,13 +22,5 @@ namespace CheckoutCom\Magento2\Block\Apm;
  */
 class Form extends \Magento\Framework\View\Element\Template
 {
-    /**
-     * Form constructor.
-     */
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        array $data = []
-    ) {
-        parent::__construct($context, $data);
-    }
+
 }
