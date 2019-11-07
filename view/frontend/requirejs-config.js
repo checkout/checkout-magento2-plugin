460a988d7e15dfceb51bfb3bd60487c822542788
david-fiaty-cko <david.fiaty@checkout.com>
2019-11-07 17:51:48 +0000

    Improved the minification strategy

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,8 @@ var config = {
     map: {
         '*': {
             framesjs: 'https://cdn.checkout.com/js/framesv2.min.js',
-            Klarna: 'https://x.klarnacdn.net/kp/lib/v1/api.js'
+            klarna: 'https://x.klarnacdn.net/kp/lib/v1/api.js',
+            googlepayjs: 'https://pay.google.com/gp/p/js/pay.js'
         }
     }
 };
