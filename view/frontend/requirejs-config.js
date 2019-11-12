50799c734094d9658cf68f95161a415403cf3f38
david-fiaty-cko <david.fiaty@checkout.com>
2019-11-12 17:04:56 +0000

    Updated the Klarna remote script mapping name

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     map: {
         '*': {
             framesjs: 'https://cdn.checkout.com/js/framesv2.min.js',
-            klarna: 'https://x.klarnacdn.net/kp/lib/v1/api.js',
+            Klarna: 'https://x.klarnacdn.net/kp/lib/v1/api.js',
             googlepayjs: 'https://pay.google.com/gp/p/js/pay.js',
             urlArgs: "bust=" + (new Date()).getTime()
         }
