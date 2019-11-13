0f09a302798991fba680ff603f9f6a3841ea89b5
david-fiaty-cko <david.fiaty@checkout.com>
2019-11-13 15:29:57 +0000

    Removed the cache buster

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,8 +3,7 @@ var config = {
         '*': {
             framesjs: 'https://cdn.checkout.com/js/framesv2.min.js',
             Klarna: 'https://x.klarnacdn.net/kp/lib/v1/api.js',
-            googlepayjs: 'https://pay.google.com/gp/p/js/pay.js',
-            urlArgs: "bust=" + (new Date()).getTime()
+            googlepayjs: 'https://pay.google.com/gp/p/js/pay.js'
         }
     }
 };
