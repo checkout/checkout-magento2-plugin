118f5e30d07ca89c26a40c54dc27246ee995c0b2
Luke.Fenn <luke.fenn@CKO-3184.local>
2020-06-08 12:51:15 +0100

    Add conditional statement to placeorder controller to filter out payment requests with no token

--- Controller/Payment/PlaceOrder.php
+++ Controller/Payment/PlaceOrder.php
@@ -151,61 +151,66 @@ class PlaceOrder extends \Magento\Framework\App\Action\Action
             // Set some required properties
             $this->data = $this->getRequest()->getParams();
 
-            // Process the request
-            if ($this->getRequest()->isAjax() && $this->quote) {
-                // Create an order
-                $order = $this->orderHandler
-                    ->setMethodId($this->data['methodId'])
-                    ->handleOrder($this->quote);
+            if ($this->isEmptyCardToken($this->data)) {
+                // Process the request
+                if ($this->getRequest()->isAjax() && $this->quote) {
+                    // Create an order
+                    $order = $this->orderHandler
+                        ->setMethodId($this->data['methodId'])
+                        ->handleOrder($this->quote);
 
-                // Process the payment
-                if ($this->orderHandler->isOrder($order)) {
-                    // Get response and success
-                    $response = $this->requestPayment($order);
+                    // Process the payment
+                    if ($this->orderHandler->isOrder($order)) {
+                        // Get response and success
+                        $response = $this->requestPayment($order);
 
-                    // Logging
-                    $this->logger->display($response);
+                        // Logging
+                        $this->logger->display($response);
 
-                    // Get the store code
-                    $storeCode = $this->storeManager->getStore()->getCode();
-                    
-                    // Process the response
-                    $api = $this->apiHandler->init($storeCode);
-                    if ($api->isValidResponse($response)) {
-                        // Get the payment details
-                        $paymentDetails = $api->getPaymentDetails($response->id);
-            
-                        // Add the payment info to the order
-                        $order = $this->utilities->setPaymentData($order, $response);
-            
-                        // Save the order
-                        $order->save();
+                        // Get the store code
+                        $storeCode = $this->storeManager->getStore()->getCode();
 
-                        // Update the response parameters
-                        $success = $response->isSuccessful();
-                        $url = $response->getRedirection();
-                    } else {
+                        // Process the response
+                        $api = $this->apiHandler->init($storeCode);
+                        if ($api->isValidResponse($response)) {
+                            // Get the payment details
+                            $paymentDetails = $api->getPaymentDetails($response->id);
 
-                        // Payment failed
-                        if(isset($response->response_code)){
-                            $message = $this->paymentErrorHandler->getErrorMessage($response->response_code);
+                            // Add the payment info to the order
+                            $order = $this->utilities->setPaymentData($order, $response);
+
+                            // Save the order
+                            $order->save();
+
+                            // Update the response parameters
+                            $success = $response->isSuccessful();
+                            $url = $response->getRedirection();
                         } else {
-                            $message = __('The transaction could not be processed.');
+
+                            // Payment failed
+                            if (isset($response->response_code)) {
+                                $message = $this->paymentErrorHandler->getErrorMessage($response->response_code);
+                            } else {
+                                $message = __('The transaction could not be processed.');
+                            }
+
+                            // Restore the quote
+                            $this->quoteHandler->restoreQuote($order->getIncrementId());
+
+                            // Handle order on failed payment
+                            $this->orderHandler->handleFailedPayment($order, $storeCode);
                         }
-
-                        // Restore the quote
-                        $this->quoteHandler->restoreQuote($order->getIncrementId());
-
-                        // Handle order on failed payment
-                        $this->orderHandler->handleFailedPayment($order, $storeCode);
+                    } else {
+                        // Payment failed
+                        $message = __('The order could not be processed.');
                     }
                 } else {
                     // Payment failed
-                    $message = __('The order could not be processed.');
+                    $message = __('The request is invalid or there was no quote found.');
                 }
             } else {
-                // Payment failed
-                $message = __('The request is invalid or there was no quote found.');
+                // No token found
+                $message = __("Please enter valid card details.");
             }
         } catch (\Exception $e) {
             $success = false;
@@ -241,5 +246,12 @@ class PlaceOrder extends \Magento\Framework\App\Action\Action
             $order->getOrderCurrencyCode(),
             $order->getIncrementId()
         );
+    }
+
+    public function isEmptyCardToken($paymentData) {
+        if (!isset($paymentData->cardToken) || empty($paymentData->cardToken) || $paymentData->cardToken = "" ) {
+            return true;
+        }
+        return false;
     }
 }
