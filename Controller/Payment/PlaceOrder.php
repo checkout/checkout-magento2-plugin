4a20adfa7cd21209dc0b02725f52aed56705174d
Luke.Fenn <luke.fenn@CKO-3184.local>
2020-09-22 12:13:13 +0100

    Fix incorrect stock level for cancelled orders

--- Controller/Payment/PlaceOrder.php
+++ Controller/Payment/PlaceOrder.php
@@ -226,6 +226,9 @@ class PlaceOrder extends \Magento\Framework\App\Action\Action
 
                             // Restore the quote
                             $this->quoteHandler->restoreQuote($order->getIncrementId());
+
+                            // Handle order on failed payment
+                            $this->orderHandler->handleFailedPayment($order);
                         }
                     } else {
                         // Payment failed
