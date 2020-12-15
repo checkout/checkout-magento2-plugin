160ee0de27e74efc632eb6b32d0b322906ab10c4
alfie-loakes-cko <69196584+alfie-loakes-cko@users.noreply.github.com>
2020-12-15 12:09:51 +0000

    save quote via cart repository

--- Model/Service/QuoteHandlerService.php
+++ Model/Service/QuoteHandlerService.php
@@ -45,9 +45,9 @@ class QuoteHandlerService
     public $quoteFactory;
 
     /**
-     * @var QuoteRepository
+     * @var CartRepositoryInterface
      */
-    public $quoteRepository;
+    public $cartRepository;
 
     /**
      * @var StoreManagerInterface
@@ -77,7 +77,7 @@ class QuoteHandlerService
         \Magento\Customer\Model\Session $customerSession,
         \Magento\Framework\Stdlib\CookieManagerInterface $cookieManager,
         \Magento\Quote\Model\QuoteFactory $quoteFactory,
-        \Magento\Quote\Model\QuoteRepository $quoteRepository,
+        \Magento\Quote\Api\CartRepositoryInterface $cartRepository,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
         \CheckoutCom\Magento2\Gateway\Config\Config $config,
@@ -87,7 +87,7 @@ class QuoteHandlerService
         $this->customerSession = $customerSession;
         $this->cookieManager = $cookieManager;
         $this->quoteFactory = $quoteFactory;
-        $this->quoteRepository = $quoteRepository;
+        $this->cartRepository = $cartRepository;
         $this->storeManager = $storeManager;
         $this->productRepository = $productRepository;
         $this->config = $config;
@@ -299,7 +299,7 @@ class QuoteHandlerService
      */
     public function getQuoteValue()
     {
-        $this->quoteRepository->save($this->getQuote());
+        $this->cartRepository->save($this->getQuote());
         return $this->getQuote()->collectTotals()->getGrandTotal();
     }
 
