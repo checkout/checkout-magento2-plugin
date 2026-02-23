498a8c29fe9cdfdf849b2476bb92b2d8f6829b36
Maxime AVELINE <maxime.aveline@dnd.fr>
2026-02-23 11:31:40 +0100

    CHECMAG2003-363: Improve payment error messages

--- Model/Service/PaymentErrorHandlerService.php
+++ Model/Service/PaymentErrorHandlerService.php
@@ -196,144 +196,207 @@ class PaymentErrorHandlerService
 
         $fundingErrors = array_fill_keys(
             [
-                "20001",
-                "20002",
-                "20005",
-                "20010",
-                "20032",
-                "20039",
-                "20040",
-                "20044",
-                "20046",
-                "20051",
-                "20052",
-                "20053",
-                "20061",
-                "20062",
-                "20065",
-                "20075",
-                "20083",
-                "20084",
-                "20085",
-                "20091",
-                "20093",
-                "200N0",
-                "200O5",
-                "200P1",
-                "200P9",
-                "200R1",
-                "200R3",
-                "200S4",
-                "200T3",
-                "200T5",
-                "20103",
-                "20108",
-                "20150",
-                "30004",
-                "30021",
-                "30022",
-                "30035",
-                "30036",
-                "30038",
+                '20001',
+                '20002',
+                '20005',
+                '20010',
+                '20032',
+                '20039',
+                '20040',
+                '20044',
+                '20046',
+                '20051',
+                '20052',
+                '20053',
+                '20061',
+                '20062',
+                '20065',
+                '20075',
+                '20083',
+                '20084',
+                '20085',
+                '20091',
+                '20093',
+                '200N0',
+                '200O5',
+                '200P1',
+                '200P9',
+                '200R1',
+                '200R3',
+                '200S4',
+                '200T3',
+                '200T5',
+                '20103',
+                '20108',
+                '20150',
+                '30004',
+                '30021',
+                '30022',
+                '30035',
+                '30036',
+                '30038',
             ],
             __(
                 'You have reached the limit allowed for this card/account, please try again with another card or payment method.'
             )
         );
 
-        $technicalErrors = array_fill_keys(
+        $technicalErrors = [
+            '20006' => __('Invalid payment request. Please check your details and try again.'),
+            '20019' => __('Your session expired. Please start your payment again.'),
+            '20023' => __('This transaction cannot be completed with this card. Please use another payment method.'),
+            '20031' => __('This bank or card is not supported. Please use another payment method.'),
+            '20058' => __('This transaction is not permitted with this card. Please use another payment method.'),
+            '20059' => __('Payment was blocked for security reasons. Please use another card.'),
+            '20060' => __('Your card was declined. Please try another card.'),
+            '20063' => __('Payment was blocked for security reasons. Please contact your bank.'),
+            '20066' => __('Payment was blocked for security reasons. Please contact your bank.'),
+            '20068' => __('The request timed out. Please try again.'),
+            '20082' => __('Security check failed. Please verify your details and try again.'),
+            '20101' => __('We couldn\'t find this account. Please check your card details.'),
+            '20102' => __('A configuration issue occurred. Please contact support.'),
+            '20104' => __('Your card could not be processed. Please use another card.'),
+            '20105' => __('Payment could not be processed. Please try again later.'),
+            '20106' => __('This currency is not supported. Please use another payment method.'),
+            '20110' => __('This payment has already been authorised.'),
+            '20112' => __('This card cannot be authenticated. Please use another card.'),
+            '20114' => __('Your session expired. Please refresh the page and try again.'),
+            '20120' => __('Please complete all required fields correctly.'),
+            '20121' => __('This transaction exceeds allowed limits. Please use another card.'),
+            '4XXXX' => __('Additional verification is required to complete your payment.')
+        ];
+
+        $technicalErrorRetry = array_fill_keys(
             [
-                "20003",
-                "20060",
-                "20102",
-                "20112",
-                "20121",
-                "30016",
-                "30017",
-                "30018",
-                "30019",
-                "20006",
-                "20009",
-                "20019",
-                "20020",
-                "20021",
-                "20022",
-                "20023",
-                "20024",
-                "20025",
-                "20026",
-                "20027",
-                "20028",
-                "20029",
-                "20030",
-                "20031",
-                "20042",
-                "20058",
-                "20064",
-                "20068",
-                "20086",
-                "20088",
-                "20089",
-                "20090",
-                "20092",
-                "20094",
-                "20095",
-                "20096",
-                "20097",
-                "20098",
-                "200T2",
-                "20101",
-                "20104",
-                "20105",
-                "20106",
-                "20107",
-                "20109",
-                "20110",
-                "20111",
-                "20113",
-                "20114",
-                "20115",
-                "20116",
-                "20117",
-                "20118",
-                "20119",
-                "20120",
-                "20123",
-                "30015",
-                "30020",
-                "20059",
-                "20063",
-                "20066",
-                "20082",
-                "30007",
-                "30034",
-                "30037",
-                "4XXXX" // Fraud response codes
+                '20003',
+                '20020',
+                '20021',
+                '20022',
+                '20024',
+                '20028',
+                '20029',
+                '20086',
+                '20088',
+                '20089',
+                '20090',
+                '20092',
+                '20095',
+                '20096',
+                '20097',
+                '20098'
             ],
-            __('Something went wrong, please try again later')
+            __('We\'re unable to process your payment right now. Please try again.')
+        );
+
+        $paymentProcessing = array_fill_keys(
+            [
+                '20009',
+                '20118'
+            ],
+            __('Your payment is being processed. Please wait.')
+        );
+
+        $invalidPaymentDetails = array_fill_keys(
+            [
+                '20025',
+                '20027',
+                '20030'
+            ],
+            __('Some payment details are incorrect. Please check and try again.')
+        );
+
+        $duplicateTransaction = array_fill_keys(
+            [
+                '20026',
+                '20094'
+            ],
+            __('This payment has already been processed.')
+        );
+
+        $invalidPaymentDetailsInformation = array_fill_keys(
+            [
+                '20042',
+                '20064',
+                '200T2'
+            ],
+            __('The payment information provided is invalid. Please check and try again.')
+        );
+
+        $missingRequiredFields = array_fill_keys(
+            [
+                '20107',
+                '20123'
+            ],
+            __('Please complete all required fields before proceeding.')
+        );
+
+        $transactionAlreadyReversed = array_fill_keys(
+            [
+                '20109',
+                '20111'
+            ],
+            __('This transaction was already reversed.')
+        );
+
+        $invalidRequest = array_fill_keys(
+            [
+                '20113',
+                '20115',
+                '20116'
+            ],
+            __('Invalid payment request. Please try again.')
+        );
+
+        $configurationError = array_fill_keys(
+            [
+                '20117',
+                '20119'
+            ],
+            __('System configuration error. Please contact support.')
+        );
+
+        $cardDeclined = array_fill_keys(
+            [
+                '30007',
+                '30015',
+                '30016',
+                '30017',
+                '30020',
+                '30034',
+                '30037'
+            ],
+            __('Your card was declined. Please try another card.')
+        );
+
+        $cardDeclinedContactBank = array_fill_keys(
+            [
+                '30018',
+                '30019'
+            ],
+            __('Your bank declined the payment. Please contact your bank.')
         );
 
         $invalidCardErrors = array_fill_keys(
             [
-                "20014",
-                "20054",
-                "20055",
-                "20056",
-                "20087",
-                "200N7",
-                "20100",
-                "30033",
-                "30041",
-                "30043",
+                '20014',
+                '20054',
+                '20055',
+                '20056',
+                '20087',
+                '200N7',
+                '20100',
+                '30033',
+                '30041',
+                '30043',
             ],
             __('It looks like your card is invalid or blocked, please try with another card')
         );
 
         $blockedCardErrors = array_fill_keys(
             [
-                "20017",
-                "20018",
-                "20057",
+                '20017',
+                '20018',
+                '20057',
             ],
             __(
                 'It looks like this transaction has been blocked due to account holder action, please contact your bank or use another card or payment method'
@@ -342,14 +405,14 @@ class PaymentErrorHandlerService
 
         $threeDsErrors = array_fill_keys(
             [
-                "20151",
-                "20152",
-                "20154",
+                '20151',
+                '20152',
+                '20154',
             ],
             __('3DS has expired or authentication failed, please try again')
         );
 
-        $messageMapper = $generalErrors + $fundingErrors + $technicalErrors + $invalidCardErrors + $blockedCardErrors + $threeDsErrors;
+        $messageMapper = $generalErrors + $fundingErrors + $technicalErrors + $technicalErrorRetry + $paymentProcessing + $invalidPaymentDetails + $duplicateTransaction + $invalidPaymentDetailsInformation + $missingRequiredFields + $transactionAlreadyReversed + $invalidRequest + $configurationError + $cardDeclined + $cardDeclinedContactBank + $invalidCardErrors + $blockedCardErrors + $threeDsErrors;
 
         return $messageMapper[$responseCode] ?? __('The transaction could not be processed');
     }
