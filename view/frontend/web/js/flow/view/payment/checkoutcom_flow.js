a0a179b129591a1e40be6da7a9557f42fa153f5e
Antoine FONTAINE <antoine.fontaine@dnd.fr>
2026-06-16 10:24:47 +0200

    CHECMAG2003-390: launch re-prepare on billing adress change

--- view/frontend/web/js/flow/view/payment/checkoutcom_flow.js
+++ view/frontend/web/js/flow/view/payment/checkoutcom_flow.js
@@ -56,7 +56,7 @@ define(
                         'applepay' : 'apple_pay'
                     },
                     currentMethod: null,
-                    currentCountryCode: null,
+                    currentBillingSignature: null,
                 },
                 reference: null,
 
@@ -89,9 +89,7 @@ define(
                     this.isLoading = true;
                     this.getFlowContextData();
 
-                    if (Utilities.getBillingAddress().country_id) {
-                        this.setCountryCode();
-                    }
+                    this.captureBillingSignature();
 
                     // Listen for saveCard event
                     document.querySelector('body').addEventListener(
@@ -101,17 +99,24 @@ define(
                         },
                     );
 
+                    Quote.billingAddress.subscribe(() => {
+                        if (Utilities.methodIsSelected(METHOD_ID) &&
+                            this.getBillingSignature() !== this.currentBillingSignature) {
+                            this.reloadFlow();
+                        }
+                    });
+
                     // Listen for Step change
                     StepNavigator.steps.subscribe((steps) => {
                         if (steps[StepNavigator.getActiveItemIndex()]['code'] === 'payment' &&
-                            Utilities.getBillingAddress().country_id !== this.currentCountryCode) {
+                            this.getBillingSignature() !== this.currentBillingSignature) {
                             this.reloadFlow();
                         }
                     });
 
                     Quote.totals.subscribe(() => {
                         const newGrandTotal = Quote.totals().base_grand_total;
-                        
+
                         if (Utilities.methodIsSelected(METHOD_ID) && newGrandTotal !== window.currentGrandTotal) {
                             this.reloadFlow();
                             window.currentGrandTotal = newGrandTotal;
@@ -120,10 +125,27 @@ define(
                 },
 
                 /**
-                 * Set current country code
+                 * Build a signature of the server-side billing identity used to drive re-prepares.
                  */
-                setCountryCode: function () {
-                    this.currentCountryCode = Utilities.getBillingAddress().country_id;
+                getBillingSignature: function () {
+                    const billing = Utilities.getBillingAddress() || {};
+                    const street = Array.isArray(billing.street) ? billing.street.join('|') : (billing.street || '');
+
+                    return [
+                        billing.firstname || '',
+                        billing.lastname || '',
+                        billing.email || Utilities.getEmail() || '',
+                        billing.country_id || '',
+                        billing.postcode || '',
+                        street
+                    ].join('::');
+                },
+
+                /**
+                 * Store the billing signature reflected by the last prepared session.
+                 */
+                captureBillingSignature: function () {
+                    this.currentBillingSignature = this.getBillingSignature();
                 },
 
                 /**
@@ -133,7 +155,7 @@ define(
                     if (!this.isLoading) {
                         this.isLoading = true;
 
-                        this.setCountryCode();
+                        this.captureBillingSignature();
                         this.sendSaveCardEvent();
 
                         if (this.flowComponent) {
